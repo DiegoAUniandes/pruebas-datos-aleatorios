d0ce151ef260499c70804431d1708edf5ebe10a4
felipe <lf.garciar1@uniandes.edu.co>
2022-11-24 21:50:03 -0500

    a priori strategy

--- cypress/support/strategy/data-pool-apriori-strategy.ts
+++ cypress/support/strategy/data-pool-apriori-strategy.ts
@@ -1,17 +1,51 @@
 import { IStrategy } from "./i-strategy";
 
+const successData = require('./datapoolapriori.json');
+
 export class DataPoolAprioriStrategy implements IStrategy{
     
-    getShortString():string { 
-        return "getShortString";
+    testData!:[]
+
+    constructor(){
+        let index = this.randomIntFromInterval(successData.length);
+        this.testData = successData[index];
     }
-    getLargeString: () => "getLargeString";
-    getUserName: () => "getUserName";
-    getUserPassword: () => "getUserPassword";
-    getEmail: () => "getEmail";
-    getNumber: () => 1
-    getTagName: () => "getTagName";
-    getNaughtyString: () => "getNaughtyString";
+
+    getShortString():string { 
+        return this.testData["getShortString"];
+    }
+
+    getLargeString():string { 
+        return this.testData["getLargeString"];
+    }
+
+    getUserName():string { 
+        return this.testData["getUserName"];
+    }
+
+    getUserPassword():string { 
+        return this.testData["getUserPassword"];
+    }
+
+    getEmail():string { 
+        return this.testData["getEmail"];
+    }
+
+    getNumber():number { 
+        return this.testData["getNumber"];
+    }
     
+    getTagName():string { 
+        return this.testData["getTagName"];
+    }
+
+    getNaughtyString():string { 
+        return this.testData["getNaughtyString"];
+    }
+    
+
+    randomIntFromInterval(max:number) {
+        return Math.floor(Math.random() * (max + 1))
+    }
     
 }
